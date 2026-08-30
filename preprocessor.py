3393c0e78525426d6c9b5bbddf455bdc5b5ed380
devsavaliya2025-sys <devsavaliya2025@gmail.com>
2026-08-30 14:27:22 +0200

    Improve WhatsApp date parsing

--- preprocessor.py
+++ preprocessor.py
@@ -4,9 +4,20 @@ import pandas as pd
 
 def preprocess(data):
 
+    # Supports common WhatsApp export formats:
+    # 17/08/25, 04:30 PM -
+    # 17/08/2025, 04:30 PM -
+    # 17/08/25, 16:30 -
+    # 17/08/2025, 16:30 -
+    # 17.08.25, 16:30 -
+    # 17.08.2025, 16:30 -
+
     pattern = (
-        r'\d{1,2}/\d{1,2}/\d{2},'
-        r'\s\d{1,2}:\d{2}\s?[apAP][mM]\s-\s'
+        r'\d{1,2}[\/\.]\d{1,2}[\/\.]\d{2,4}'
+        r',\s'
+        r'\d{1,2}:\d{2}'
+        r'(?:\s?[apAP][mM])?'
+        r'\s-\s'
     )
 
     messages = re.split(
@@ -19,6 +30,26 @@ def preprocess(data):
         data
     )
 
+    # If no WhatsApp messages were detected
+    if len(messages) == 0 or len(dates) == 0:
+
+        return pd.DataFrame(
+            columns=[
+                'date',
+                'user',
+                'message',
+                'only_date',
+                'year',
+                'month_num',
+                'month',
+                'day',
+                'day_name',
+                'hour',
+                'minute',
+                'period'
+            ]
+        )
+
     df = pd.DataFrame({
         'user_message': messages,
         'message_date': dates
@@ -28,17 +59,28 @@ def preprocess(data):
     # CONVERT DATE
     # =========================
 
-    df['message_date'] = df[
-        'message_date'
-    ].str.replace(
-        ' - ',
-        '',
-        regex=False
+    df['message_date'] = (
+        df['message_date']
+        .astype(str)
+        .str.replace(
+            ' - ',
+            '',
+            regex=False
+        )
     )
 
+    # Try automatic date parsing
     df['message_date'] = pd.to_datetime(
         df['message_date'],
-        format='%d/%m/%y, %I:%M %p'
+        dayfirst=True,
+        errors='coerce'
+    )
+
+    # Remove invalid dates
+    df = df.dropna(
+        subset=['message_date']
+    ).reset_index(
+        drop=True
     )
 
     df.rename(
@@ -53,22 +95,23 @@ def preprocess(data):
     # =========================
 
     users = []
-    messages = []
+    message_list = []
 
     for message in df['user_message']:
 
         entry = re.split(
             r'([\w\W]+?):\s',
-            message
+            message,
+            maxsplit=1
         )
 
-        if entry[1:]:
+        if len(entry) >= 3:
 
             users.append(
                 entry[1]
             )
 
-            messages.append(
+            message_list.append(
                 entry[2]
             )
 
@@ -78,12 +121,12 @@ def preprocess(data):
                 'group_notification'
             )
 
-            messages.append(
+            message_list.append(
                 entry[0]
             )
 
     df['user'] = users
-    df['message'] = messages
+    df['message'] = message_list
 
     df.drop(
         columns=['user_message'],
@@ -126,14 +169,33 @@ def preprocess(data):
         df['date'].dt.minute
     )
 
+    # =========================
+    # TIME PERIOD
+    # =========================
+
     period = []
-    for hour in df[['day_name', 'hour']]['hour']:
+
+    for hour in df['hour']:
+
         if hour == 23:
-            period.append(str(hour) + "-" + str('00'))
+
+            period.append(
+                '23-00'
+            )
+
         elif hour == 0:
-            period.append(str('00') + "-" + str(hour + 1))
+
+            period.append(
+                '00-01'
+            )
+
         else:
-            period.append(str(hour) + "-" + str(hour + 1))
+
+            period.append(
+                str(hour)
+                + "-"
+                + str(hour + 1)
+            )
 
     df['period'] = period
 
